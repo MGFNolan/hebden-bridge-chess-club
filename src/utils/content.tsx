0dd80f701c1389a6344ce40747bda6b3b3d5d20f
Matthew N <mn.gf@outlook.com>
2025-06-14 16:49:04 +0100

    2 - content and types for nav bar

---
+++ src/utils/content.tsx
@@ -0,0 +1,31 @@
+import { type NavLink } from './contentTypes';
+
+const NavLinks: NavLink[] = [
+    {
+        id: 1,
+        href: '/',
+        text: 'Home',
+    },
+    {
+        id: 2,
+        href: '#what-we-offer',
+        text: 'What We Offer',
+    },
+    {
+        id: 3,
+        href: '#club-information',
+        text: 'Club Information',
+    },
+    {
+        id: 4,
+        href: '#club-history',
+        text: 'Club History',
+    },
+    {
+        id: 5,
+        href: '#contact',
+        text: 'Contact Us',
+    },
+];
+
+export { NavLinks };
