5200fa99c31040dca86ab6060351961a9deec571
Matthew N <mn.gf@outlook.com>
2025-06-24 16:39:43 +0100

    5: accessibility

--- src/components/Club Information/ClubInformation.tsx
+++ src/components/Club Information/ClubInformation.tsx
@@ -3,10 +3,19 @@ import { ClubInformationContent } from "../../utils/content";
 
 export default function ClubInformation() {
     return (
-        <section id="club-information" className="club-information">
+        <section
+            id="club-information"
+            className="club-information"
+            aria-labelledby="club-information-heading"
+        >
             <div className="club-information__wrapper">
-                <h2 className="club-information__title">Club Information</h2>
-                <ul className="club-information__list">
+                <h2
+                    className="club-information__title"
+                    id="club-information-heading"
+                >
+                    Club Information
+                </h2>
+                <ul className="club-information__list" role="list">
                     {ClubInformationContent.map((info) => (
                         <ClubInformationIndividual
                             information={info}
--- src/components/Club Information/ClubInformationIndividual.tsx
+++ src/components/Club Information/ClubInformationIndividual.tsx
@@ -8,10 +8,22 @@ export default function ClubInformationIndividual({
     information,
 }: Information) {
     return (
-        <li key={information.id} className="info-individual">
+        <li
+            className="info-individual"
+            role="listitem"
+            aria-label={`Club information: ${information.title}`}
+        >
             <div className="info-individual__wrapper">
-                <h3 className="info-individual__title">{information.title}</h3>
-                <p className="info-individual__desciption">
+                <h3
+                    className="info-individual__title"
+                    id={`info-title-${information.id}`}
+                >
+                    {information.title}
+                </h3>
+                <p
+                    className="info-individual__desciption"
+                    aria-describedby={`info-title-${information.id}`}
+                >
                     {information.description}
                 </p>
             </div>
