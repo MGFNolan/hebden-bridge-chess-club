d2315be05b88685d4a69e90cd1739dfc03b5a037
Matthew N <mn.gf@outlook.com>
2025-07-04 20:26:07 +0100

    15: create basic structure and styling

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,63 @@
+import { ContactOptionsContent } from "../utils/content";
+
+export default function ContactForm() {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const data = new FormData(form);
+        const formObject = Object.fromEntries(data.entries());
+        console.log("Form Data:", formObject);
+        // Send with EmailJS or other handler here
+    };
+
+    return (
+        <section id="contact-us">
+            <div className="container">
+                <h2>Contact Us</h2>
+                <form onSubmit={handleSubmit} className="contact-us">
+                    <label className="contact-us__label" htmlFor="name">
+                        Your Name
+                    </label>
+                    <input
+                        autoComplete="name"
+                        className="contact-us__name"
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Write your name here"
+                    />
+
+                    <label className="contact-us__label" htmlFor="subject">
+                        Subject
+                    </label>
+                    <select
+                        id="subject"
+                        name="subject"
+                        className="contact-us__subject"
+                    >
+                        {ContactOptionsContent.map((option) => (
+                            <option value={option.option} key={option.id}>
+                                {option.option}
+                            </option>
+                        ))}
+                    </select>
+
+                    <label className="contact-us__label" htmlFor="message">
+                        Message
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder="Write your message here"
+                        style={{ height: "200px" }}
+                        className="contact-us__message"
+                    />
+
+                    <button type="submit" className="btn--primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+}
