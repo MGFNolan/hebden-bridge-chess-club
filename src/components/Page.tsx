908e68d64526da091c6765f26845b7eeff245b2b
Matthew N <mn.gf@outlook.com>
2025-07-11 00:05:32 +0100

    18: add skip to main

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,18 @@
-import { type ReactElement } from 'react';
+import { type ReactElement } from "react";
 
 interface PageProps {
     children: ReactElement[];
 }
 
 function Page(props: PageProps) {
-    return <div id="main-content">{props.children}</div>;
+    return (
+        <>
+            <a href="#main-content" className="screen-reader-text">
+                Skip to main content
+            </a>
+            <div id="main-content">{props.children}</div>
+        </>
+    );
 }
 
 export default Page;
