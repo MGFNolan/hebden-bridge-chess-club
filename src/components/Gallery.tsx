8d2da7a9a87de669d6dce8b1fe4c3840851f2254
Matthew N <mn.gf@outlook.com>
2025-10-22 17:37:29 +0100

    27: minor semantic change for what is within a container div

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -26,69 +26,70 @@ export default function Gallery() {
                 <h2 className="gallery__title" id="gallery-heading">
                     Gallery
                 </h2>
-            </div>
-            <div className="gallery__swiper" id="main-swiper-container">
-                <Swiper
-                    id="main-swiper"
-                    onSwiper={setMainSwiper}
-                    loop={true}
-                    spaceBetween={10}
-                    navigation={true}
-                    thumbs={{ swiper: thumbsSwiper }}
-                    modules={[A11y, FreeMode, Navigation, Thumbs]}
-                    className="gallery-swiper"
-                    aria-label="Image gallery, main display"
-                    a11y={{
-                        prevSlideMessage: "Previous image",
-                        nextSlideMessage: "Next image",
-                    }}
-                >
-                    {GalleryContent.map((image) => (
-                        <SwiperSlide key={image.id}>
-                            <img
-                                loading="lazy"
-                                src={image.image}
-                                alt={image.alt}
-                            />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-                <Swiper
-                    onSwiper={setThumbsSwiper}
-                    loop={true}
-                    spaceBetween={10}
-                    slidesPerView={3.5}
-                    freeMode={true}
-                    watchSlidesProgress={true}
-                    modules={[A11y, FreeMode, Navigation, Thumbs]}
-                    className="gallery-thumbs"
-                    aria-label="Thumbnail image navigation"
-                    a11y={{
-                        slideRole: "button",
-                        slideLabelMessage:
-                            "View image {{index}} of {{slidesLength}}",
-                    }}
-                >
-                    {GalleryContent.map((image, index) => (
-                        <SwiperSlide
-                            key={image.id}
-                            tabIndex={0}
-                            onClick={() => handleThumbnailClick(index)}
-                            onKeyDown={(e) => {
-                                if (e.key === "Enter" || e.key === " ") {
-                                    handleThumbnailClick(index);
-                                    e.preventDefault();
-                                }
-                            }}
-                        >
-                            <img
-                                loading="lazy"
-                                src={image.image}
-                                alt={image.alt}
-                            />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+
+                <div className="gallery__swiper" id="main-swiper-container">
+                    <Swiper
+                        id="main-swiper"
+                        onSwiper={setMainSwiper}
+                        loop={true}
+                        spaceBetween={10}
+                        navigation={true}
+                        thumbs={{ swiper: thumbsSwiper }}
+                        modules={[A11y, FreeMode, Navigation, Thumbs]}
+                        className="gallery-swiper"
+                        aria-label="Image gallery, main display"
+                        a11y={{
+                            prevSlideMessage: "Previous image",
+                            nextSlideMessage: "Next image",
+                        }}
+                    >
+                        {GalleryContent.map((image) => (
+                            <SwiperSlide key={image.id}>
+                                <img
+                                    loading="lazy"
+                                    src={image.image}
+                                    alt={image.alt}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <Swiper
+                        onSwiper={setThumbsSwiper}
+                        loop={true}
+                        spaceBetween={10}
+                        slidesPerView={3.5}
+                        freeMode={true}
+                        watchSlidesProgress={true}
+                        modules={[A11y, FreeMode, Navigation, Thumbs]}
+                        className="gallery-thumbs"
+                        aria-label="Thumbnail image navigation"
+                        a11y={{
+                            slideRole: "button",
+                            slideLabelMessage:
+                                "View image {{index}} of {{slidesLength}}",
+                        }}
+                    >
+                        {GalleryContent.map((image, index) => (
+                            <SwiperSlide
+                                key={image.id}
+                                tabIndex={0}
+                                onClick={() => handleThumbnailClick(index)}
+                                onKeyDown={(e) => {
+                                    if (e.key === "Enter" || e.key === " ") {
+                                        handleThumbnailClick(index);
+                                        e.preventDefault();
+                                    }
+                                }}
+                            >
+                                <img
+                                    loading="lazy"
+                                    src={image.image}
+                                    alt={image.alt}
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
             </div>
         </section>
     );
