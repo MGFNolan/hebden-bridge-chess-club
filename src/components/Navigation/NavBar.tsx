455f798710bc6f07ac0cc3a1aadfc820b8bfb85a
Matthew N <mn.gf@outlook.com>
2025-11-29 18:11:16 +0000

    38: make navbar disappear on scroll

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -3,8 +3,28 @@ import Hamburger from "./../Icons/Hamburger.tsx";
 import LogoIcon from "./../Icons/LogoIcon.tsx";
 
 export default function NavBar() {
+    let prevScrollPos = window.pageYOffset;
+
+    window.onscroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const navbar = document.getElementById("navbar");
+
+        if (navbar) {
+            // Transition opacity instead of top
+            navbar.style.transition = "opacity 0.5s ease";
+
+            if (prevScrollPos > currentScrollPos) {
+                navbar.style.opacity = "1";
+            } else {
+                navbar.style.opacity = "0";
+            }
+        }
+
+        prevScrollPos = currentScrollPos;
+    };
+
     return (
-        <nav className="nav" aria-label="Main navigation">
+        <nav className="nav" aria-label="Main navigation" id="navbar">
             <div className="nav__group">
                 <a href="/" className="nav__group__logo" aria-label="Home">
                     <LogoIcon />
