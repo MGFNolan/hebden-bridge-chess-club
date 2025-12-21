ae09500d73653e80f7915c0c56d6896bf5099c21
Matthew N <mn.gf@outlook.com>
2025-12-21 15:47:31 +0000

    62: remove classes not being used

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -5,7 +5,7 @@ export default function Error(props: { children: string }) {
         <div className="flex flex-col items-center justify-center bg-black place-content-center p-4 rounded-sm border-red-600 border-6 border-dashed">
             <ErrorIcon className="fill-red-600 py-3 size-24" />
             <div className="flex max-w-124 flex-col justify-center gap-y-2 text-center">
-                <h2 className="font-semibold text-red-600">Oh no!</h2>
+                <h2>Oh no!</h2>
                 <p className="font-light text-[var(--secondary-colour)]">
                     {props.children} Try re-loading the page or returning later.
                     If the issue persists, please use the{" "}
