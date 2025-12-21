a728980828749cdfc9e445e0ef22d3fb2db69c2b
Matthew N <mn.gf@outlook.com>
2025-12-21 16:03:30 +0000

    62: fix error component spinner

--- src/components/Icons/Spinner.tsx
+++ src/components/Icons/Spinner.tsx
@@ -1,13 +1,22 @@
 export default function Spinner() {
-  return (
-    <div className="relative box-border inline-block size-20 bg-white">
-      <div className="animate-spinn-45 border-t-grey-300 absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent" />
+    return (
+        <div className="relative box-border inline-block size-20">
+            <div
+                className="animate-spin border-t-black absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent"
+                style={{ animationDuration: "1.5s" }}
+            />
 
-      <div className="animate-spinn-30 border-t-grey-300 absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent" />
+            <div
+                className="animate-spin border-t-black absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent"
+                style={{ animationDuration: "2s" }}
+            />
 
-      <div className="animate-spinn-15 border-t-grey-300 absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent" />
+            <div
+                className="animate-spin border-t-black absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent"
+                style={{ animationDuration: "2.5s" }}
+            />
 
-      <div className="animate-spinn border-t-grey-300 absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent" />
-    </div>
-  );
+            <div className="animate-spin border-t-black absolute m-2 box-border block size-16 rounded-full border-8 border-solid border-r-transparent border-b-transparent border-l-transparent" />
+        </div>
+    );
 }
