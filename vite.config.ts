8db290e3ccf60e3a00330ea823674f4d60cba0b9
MGFN <mn.gf@outlook.com>
2026-05-12 23:03:31 +0100

    remove base path from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/hebden-bridge-chess-club/",
     plugins: [react(), tailwindcss()],
 });
